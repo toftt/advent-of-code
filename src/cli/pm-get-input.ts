313f65859d3335534d1443b9493c3da3fc636851
Joachim Toft <joachim.toft@hey.com>
2021-12-02 06:41:26 +0100

    command to fetch input

---
+++ src/cli/pm-get-input.ts
@@ -0,0 +1,43 @@
+import path from "path";
+import fs from "fs";
+import { Command } from "commander";
+import axios from "axios";
+import { paths } from "paths";
+
+const year = process.env.YEAR;
+const sessionCookie = process.env.SESSION_COOKIE;
+
+const program = new Command();
+program.parse(process.argv);
+
+const [day] = program.args;
+
+if (!day) {
+  throw new Error("no day");
+}
+if (!year) {
+  throw new Error("no year");
+}
+if (!sessionCookie) {
+  throw new Error("no session cookie");
+}
+
+const targetFolder = path.join(paths.srcFolder, year, `day${day}`);
+
+if (!fs.existsSync(targetFolder)) {
+  throw new Error(`Can't find folder ${targetFolder}`);
+}
+
+axios
+  .get<string>(`https://adventofcode.com/${year}/day/${day}/input`, {
+    headers: {
+      cookie: `session=${sessionCookie}`,
+    },
+  })
+  .then((response) => {
+    const inputPath = path.join(targetFolder, "input");
+    fs.writeFileSync(inputPath, response.data.trim());
+  })
+  .catch((error) => {
+    console.error(error);
+  });
--- src/cli/pm.ts
+++ src/cli/pm.ts
@@ -7,5 +7,6 @@ const program = new Command();
 
 program.command("solve <day>.<part>", "Run solution for a specified day.");
 program.command("generate <type>", "Generate a solution folder or run file.");
+program.command("get-input <day>", "Fetch the input for a specified day,");
 
 program.parse(process.argv);
