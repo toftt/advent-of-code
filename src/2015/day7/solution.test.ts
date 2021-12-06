2408630227145943a92d11685b37224e2de703ec
Joachim Toft <joachim.toft@hey.com>
2021-12-06 21:50:20 +0100

    2015 - day 7

--- src/2015/day7/solution.test.ts
+++ src/2015/day7/solution.test.ts
@@ -2,10 +2,10 @@ import { part1, part2 } from "./solution";
 
 describe("2015 - day 7", () => {
   it("solves part 1", () => {
-    expect(part1()).toEqual(0);
+    expect(part1()).toEqual(46065);
   });
 
   it("solves part 2", () => {
-    expect(part2()).toEqual(0);
+    expect(part2()).toEqual(14134);
   });
 });
--- src/2015/day7/solution.ts
+++ src/2015/day7/solution.ts
@@ -5,7 +5,7 @@ const binOp = /^(\w+) (\w+) (\w+) -> (\w+)$/;
 const unOp = /^(\w+) (\w+) -> (\w+)$/;
 
 type Operator = "AND" | "OR" | "RSHIFT" | "LSHIFT";
-type ImmOrRegister = number | string;
+type ImmOrRegister = string;
 type Register = string;
 type Source =
   | {
@@ -19,20 +19,11 @@ type Source =
       rhs: ImmOrRegister;
     }
   | { src: ImmOrRegister; type: "unOp" };
-type Instruction =
-  | {
-      src: Source;
-      dst: Register;
-    }
-  | {
-      src: Source;
-      dst: Register;
-    }
-  | {
-      // only NOT
-      src: Source;
-      dst: Register;
-    };
+
+type Instruction = {
+  src: Source;
+  dst: Register;
+};
 
 const parseInstruction = (s: string): Instruction => {
   let match: ReturnType<string["match"]> = null;
@@ -57,11 +48,58 @@ const parseInstruction = (s: string): Instruction => {
 
   if ((match = s.match(unOp)) !== null) {
     return {
-      src: { type: "unOp", src: match[1] },
-      dst: match[2],
+      src: { type: "unOp", src: match[2] },
+      dst: match[3],
     };
   }
   throw new Error();
+};
+
+const isNumber = (s: string) => /^\d+$/.test(s);
+
+const cache = new Map<Register, number>();
+const getSource = (m: Map<Register, Source>, r: Register): number => {
+  if (isNumber(r)) return parseInt(r);
+  if (cache.has(r)) {
+    return cache.get(r)!;
+  }
+
+  const src = m.get(r)!;
+
+  let result: number | null = null;
+
+  switch (src.type) {
+    case "imm": {
+      result = getSource(m, src.value);
+      break;
+    }
+    case "binOp": {
+      const lhs = getSource(m, src.lhs);
+      const rhs = getSource(m, src.rhs);
+
+      if (src.op === "AND") result = lhs & rhs;
+      if (src.op === "OR") result = lhs | rhs;
+      if (src.op === "LSHIFT") result = lhs << rhs;
+      if (src.op === "RSHIFT") result = lhs >> rhs;
+      break;
+    }
+    case "unOp": {
+      const val = getSource(m, src.src);
+      result = ~val;
+      break;
+    }
+
+    default: {
+      throw new Error("invalid type");
+    }
+  }
+  if (result !== null) {
+    cache.set(r, result);
+    return result;
+  } else {
+    console.log(r);
+    throw new Error("no result");
+  }
 };
 
 export const part1 = (useTestData: boolean = false): number => {
@@ -75,14 +113,24 @@ export const part1 = (useTestData: boolean = false): number => {
     m.set(ins.dst, ins.src);
   });
 
-  // console.log(m);
-
-  return 0;
+  return getSource(m, "a") & 0xffff;
 };
 
 export const part2 = (useTestData: boolean = false): number => {
   const input = readInput(useTestData);
+  const lines = lineify(input);
+  const instructions = lines.map(parseInstruction);
 
-  // solution here
-  return 0;
+  const m = new Map<Register, Source>();
+
+  instructions.forEach((ins) => {
+    m.set(ins.dst, ins.src);
+  });
+
+  const aSignal = getSource(m, "a") & 0xffff;
+
+  m.set("b", { type: "imm", value: aSignal.toString() });
+  cache.clear();
+
+  return getSource(m, "a") & 0xffff;
 };
