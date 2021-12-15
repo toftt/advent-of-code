122bda2c3b8f73f43b3d9a76cfae26f0b21b53f1
Joachim Toft <joachim.toft@hey.com>
2021-12-15 08:15:04 +0100

    dont run those tests

--- src/2021/day15/solution.test.ts
+++ src/2021/day15/solution.test.ts
@@ -1,11 +1,11 @@
 import { part1, part2 } from "./solution";
 
 describe("2021 - day 15", () => {
-  it("solves part 1", () => {
+  it.skip("solves part 1", () => {
     expect(part1()).toEqual(366);
   });
 
-  it("solves part 2", () => {
+  it.skip("solves part 2", () => {
     expect(part2()).toEqual(2829);
   });
 });
